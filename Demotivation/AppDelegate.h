476e56bf3c64e4f6b5b5fa1f4dddd094d9386329
Andrew Mauney <andrewmauney@macbookpro-b0e9.elon.edu>
2012-05-09 18:34:40 -0400

    Initial Commit

---
+++ Demotivation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Demotivation
+//
+//  Created by Andrew Mauney on 5/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Demotivation/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Demotivation
+//
+//  Created by Andrew Mauney on 5/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    [pool release];
+    return retVal;
+}
